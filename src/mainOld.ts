54bdf4bda03c87d39f467c709a7c876f7e03eb5c
Gregor Obreza <gobreza@gmail.com>
2023-06-29 20:12:34 +0200

    blob wip

--- src/mainOld.ts
+++ src/mainOld.ts
@@ -14,7 +14,7 @@ let camera: THREE.PerspectiveCamera,
   stats: Stats,
   circle: THREE.Object3D<THREE.Event>,
   skelet: THREE.Object3D<THREE.Event>,
-  blob : THREE.Mesh<THREE.SphereGeometry, THREE.MeshNormalMaterial>
+  particle: THREE.Object3D<THREE.Event>;
 
 const mouse = new THREE.Vector2();
 const target = new THREE.Vector2();
@@ -45,9 +45,31 @@ function init() {
 
   circle = new THREE.Object3D();
   skelet = new THREE.Object3D();
+  particle = new THREE.Object3D();
 
+  // scene.add(circle);
+  // scene.add(skelet);
+  scene.add(particle);
+
+  const geometry = new THREE.TetrahedronGeometry(2, 0);
   const geom = new THREE.IcosahedronGeometry(7, 1);
   const geom2 = new THREE.IcosahedronGeometry(15, 1);
+
+  const material2 = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    // shading: THREE.FlatShading,
+    flatShading: true,
+  });
+
+  for (let i = 0; i < 500; i++) {
+    const mesh = new THREE.Mesh(geometry, material2);
+    mesh.position
+      .set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
+      .normalize();
+    mesh.position.multiplyScalar(90 + Math.random() * 200);
+    mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+    particle.add(mesh);
+  }
 
   const mat = new THREE.MeshPhongMaterial({
     color: 0xffffff,
@@ -55,38 +77,27 @@ function init() {
   });
 
   const mat2 = new THREE.MeshPhongMaterial({
-    color: 0x080c1d,
+    color: 0xffffff,
     wireframe: true,
     side: THREE.DoubleSide,
   });
 
   const planet = new THREE.Mesh(geom, mat);
-  planet.scale.x = planet.scale.y = planet.scale.z = 10;
+  planet.scale.x = planet.scale.y = planet.scale.z = 8;
   // planet.translateX(150)
   circle.add(planet);
 
   const planet2 = new THREE.Mesh(geom2, mat2);
-  planet2.scale.x = planet2.scale.y = planet2.scale.z = 7;
+  planet2.scale.x = planet2.scale.y = planet2.scale.z = 5;
   // planet2.translateX(150)
   skelet.add(planet2);
 
   const group = new THREE.Group();
   group.add(circle);
   group.add(skelet);
-  group.position.z = 0;
-  group.translateX(100);
+  group.translateX(100)
 
   scene.add(group);
-
-  //blob
-  const blob_geometry = new THREE.SphereGeometry(10, 128, 128);
-  const material = new THREE.MeshNormalMaterial();
-
-  blob = new THREE.Mesh(blob_geometry, material);
-  blob.scale.x = blob.scale.y = blob.scale.z = 7;
-  blob.position.x = (-100);
-  blob.position.z = -500
-  scene.add(blob);
 
   const ambientLight = new THREE.AmbientLight(0x999999);
   scene.add(ambientLight);
@@ -102,6 +113,40 @@ function init() {
   scene.add(lights[1]);
   scene.add(lights[2]);
 
+  const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const sphereGeometry = new THREE.SphereGeometry(10);
+
+  const material = new THREE.MeshNormalMaterial();
+
+  for (let i = 0; i < 500; i++) {
+    const box = new THREE.Mesh(boxGeometry, material);
+    // box.position.x = Math.random() * (280 - 140);
+    // box.position.y = Math.random() * (280 - 140);
+    // box.position.z = Math.random() * (200 - 100);
+    // box.rotation.x = Math.random() * 2 * Math.PI;
+    // box.rotation.y = Math.random() * 2 * Math.PI;
+    // box.rotation.z = Math.random() * 2 * Math.PI;
+    box.position
+      .set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
+      .normalize();
+    box.position.multiplyScalar(90 + Math.random() * 700);
+    box.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+    // box.updateMatrix();
+    // box.matrixAutoUpdate = false;
+    scene.add(box);
+    boxes.push(box);
+  }
+
+  const sphere = new THREE.Mesh(sphereGeometry, material);
+  sphere.position.x = Math.random() * (70 - 35);
+  sphere.position.y = 0;
+  sphere.position.z = 0;
+  // sphere.rotation.x = Math.random() * 2 * Math.PI;
+  // sphere.rotation.y = Math.random() * 2 * Math.PI;
+  // sphere.rotation.z = Math.random() * 2 * Math.PI;
+  scene.add(sphere);
+  spheres.push(sphere);
+
   renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#bg") || undefined,
     antialias: true,
@@ -109,7 +154,7 @@ function init() {
   renderer.setClearColor(0x000000, 0.0);
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.position.setZ(280);
+  camera.position.setZ(500);
 
   stats = new Stats();
   document.body.appendChild(stats.dom);
@@ -130,6 +175,18 @@ function onMouseWheel(event: WheelEvent) {
   camera.position.z += event.deltaY * 0.05; // move camera along z-axis
 }
 
+// function onMouseWheelSmooth(event: any) {
+//   // camera.position.z += event.deltaY * 0.02; // move camera along z-axis
+// console.log(event)
+//   const delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail))); // Get scroll direction and speed
+//   const targetPosition = {z: camera.position.z + delta * 0.02}
+//   console.log(targetPosition)
+//   // Animate the camera position
+//   cameraTween.stop(); // Stop any ongoing tween
+//   cameraTween.to(targetPosition, 200).start(); // Adjust the duration (200) to match the tween object's duration
+//   renderer.render(scene, camera);
+// }
+
 function onResize() {
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -140,6 +197,18 @@ function onResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(width, height);
 }
+
+// function createControls(camera: THREE.PerspectiveCamera) {
+//   controls = new TrackballControls(camera, renderer.domElement);
+
+//   controls.rotateSpeed = 0.5;
+//   // controls.noRotate = true
+//   controls.noPan = true
+//   controls.zoomSpeed = 1.0;
+//   // controls.panSpeed = 0.8;
+
+//   controls.keys = ["KeyA", "KeyS", "KeyD"];
+// }
 
 function onWindowResize() {
   const aspect = window.innerWidth / window.innerHeight;
@@ -152,34 +221,28 @@ function onWindowResize() {
   controls.handleResize();
 }
 
-function blobAnimation(){
-// change '0.003' for more aggressive animation
-const time = performance.now() * 0.001;
-//console.log(time)
-
-//go through vertices here and reposition them
-
-// change 'k' value for more spikes
-const k = 1;
-for (let i = 0; i < blob.geometry.vertices.length; i++) {
-    let p = blob.geometry.vertices[i];
-    p.normalize().multiplyScalar(1 + 0.2 * noise.perlin3(p.x * k + time, p.y * k, p.z * k));
-}
-blob.geometry.computeVertexNormals();
-blob.geometry.normalsNeedUpdate = true;
-blob.geometry.verticesNeedUpdate = true;
-
-
-}
-
 function animate() {
   requestAnimationFrame(animate);
+
+  const timer = 0.0001 * Date.now();
+
+  for (let i = 0, il = boxes.length; i < il; i++) {
+    const box = boxes[i];
+
+    box.position.x = 30 * Math.cos(timer * 0.03 + i);
+    box.position.y = 30 * Math.sin(timer * 0.05 + i * 1.1);
+
+    box.rotation.x = 30 * Math.cos(timer * 0.05 + i);
+    box.rotation.y = 30 * 1.2 * Math.sin(timer * 0.05 + i);
+    box.rotation.z = 30 * Math.cos(timer * 0.05 + i);
+  }
 
   target.x = (1 - mouse.x) * 0.0002;
   target.y = (1 - mouse.y) * 0.0002;
 
-  camera.rotation.x += 0.03 * (target.y - camera.rotation.x);
-  camera.rotation.y += 0.03 * (target.x - camera.rotation.y);
+  camera.rotation.x += 0.05 * (target.y - camera.rotation.x);
+  camera.rotation.y += 0.05 * (target.x - camera.rotation.y);
+
 
   circle.rotation.x -= 0.002;
   circle.rotation.y -= 0.003;

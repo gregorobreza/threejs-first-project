d1b5975fb76b6d602a68b333941334724da4ceff
Gregor Obreza <gobreza@gmail.com>
2023-07-04 19:21:00 +0200

    wip created header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "rich-black": "#080C1D",
+      "indigo-dye": "#0B447A",
+      "rose": "#FE0172",
+      "columbia-blue": "#C4DEE8",
+      "baby-powder": "#FFFCF9",
+    },
+    extend: {
+
+
+    },
   },
   plugins: [],
 }
